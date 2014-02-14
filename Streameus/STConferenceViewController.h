6838d49be85c6be23a2a7ecea3543d1256dcc510
Anas AIT ALI <aitali.anas@gmail.com>
2014-02-15 01:38:26 +1100

    STREAM-85 #time 1h 50m #comment Ajout de la vue conference + vue recherche + liaisons manquantes #done

---
+++ Streameus/STConferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  STConferenceViewController.h
+//  Streameus
+//
+//  Created by Anas Ait Ali on 15/02/2014.
+//  Copyright (c) 2014 Streameus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STConferenceViewController : UIViewController
+
+@end
---
+++ Streameus/STConferenceViewController.m
@@ -0,0 +1,38 @@
+//
+//  STConferenceViewController.m
+//  Streameus
+//
+//  Created by Anas Ait Ali on 15/02/2014.
+//  Copyright (c) 2014 Streameus. All rights reserved.
+//
+
+#import "STConferenceViewController.h"
+
+@interface STConferenceViewController ()
+
+@end
+
+@implementation STConferenceViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
