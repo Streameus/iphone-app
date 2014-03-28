788d990e748fb7a00fb3379e94efeb45266ef4d8
Anas AIT ALI <aitali.anas@gmail.com>
2014-03-29 00:14:52 +1100

    STREAM-176 #time 1h #comment Creation d’un nouveau projet qui sera le SDK + inclusion au projet principale + build de la librairie statique #in-progress

---
+++ StreameusAPI/StreameusAPI/StreameusAPI.h
@@ -0,0 +1,13 @@
+//
+//  StreameusAPI.h
+//  StreameusAPI
+//
+//  Created by Anas Ait Ali on 28/03/2014.
+//  Copyright (c) 2014 Streameus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StreameusAPI : NSObject
+
+@end
---
+++ StreameusAPI/StreameusAPI/StreameusAPI.m
@@ -0,0 +1,13 @@
+//
+//  StreameusAPI.m
+//  StreameusAPI
+//
+//  Created by Anas Ait Ali on 28/03/2014.
+//  Copyright (c) 2014 Streameus. All rights reserved.
+//
+
+#import "StreameusAPI.h"
+
+@implementation StreameusAPI
+
+@end
